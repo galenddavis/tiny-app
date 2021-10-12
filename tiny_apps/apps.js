c3035eee8d0c13d3b89458298ae335a23c7b31b2
trash <73361653+galenddavis@users.noreply.github.com>
2021-10-12 18:22:19 -0400

    Refactor app switching to rely on imported object with colors and names attached

---
+++ tiny_apps/apps.js
@@ -0,0 +1,38 @@
+let apps = {
+    app: {
+        name: 'app',
+        color: '#3df9ea',
+        // font: 'Ropa Sans', sansserif
+    },
+    blog: {
+        name: 'blog',
+        color: '#316436',
+        // font: 'Felipa', cursive
+    },
+    shop: {
+        name: 'shop',
+        color: '#b95358',
+        // font: 'Major Mono Display', monospace
+    },
+    note: {
+        name: 'note',
+        color: '#7dcfa8',
+        // font: 'Fredericka the Great', cursive
+    },
+    game: {
+        name: 'game',
+        color: '#e624af',
+        // font: 'DotGothic16', sansserif
+    },
+    grave: {
+        name: 'grave',
+        color: 'red',
+        background: '#000000'
+        // font: 'Creepster', cursive
+    },
+    art: {
+        name: 'art',
+        color: '#5433be',
+        // font: 'Permanent Marker', cursive
+    },
+}
--- tiny_apps/index.js
+++ tiny_apps/index.js
@@ -1,12 +1,14 @@
-let appList = [
-    "app",
-    "blog",
-    "shop",
-    "note",
-    "game",
-    "grave",
-    "art"
-]
+// let appList = [
+//     "app",
+//     "blog",
+//     "shop",
+//     "note",
+//     "game",
+//     "grave",
+//     "art"
+// ]
+
+let appList = Object.values(apps);
 
 const display = document.getElementById('display')
 const appTitle = document.getElementById('app-name')
@@ -32,50 +34,75 @@ debugger
 // }
 
 const cleanUp = () => {
+    document.body.style.backgroundColor = '#FFFFFF'
     display.innerHTML = '';
+}
+
+// const setDisplay = () => {
+//     let selectedApp = appList[0];
+//     appTitle.innerHTML = selectedApp;
+//     if (selectedApp === 'art') {
+//         cleanUp()
+//         generateArt('#5433be');
+//         appTitle.style.color = '#5433be';
+//         appTitle.style.fontFamily = 'Permanent Marker', cursive;
+//     } else if (selectedApp === 'game') {
+//         cleanUp()
+//         generateGame('#e624af');
+//         appTitle.style.color = '#e624af';
+//         appTitle.style.fontFamily = 'DotGothic16', sans-serif; 
+//     } else if (selectedApp === 'app') {
+//         cleanUp()      
+//          comingSoon('#3df9ea', 'Ropa Sans')
+//         appTitle.style.color = '#3df9ea';
+//         appTitle.style.fontFamily = 'Ropa Sans', sans-serif;
+//     } else if (selectedApp === 'blog') {
+//         cleanUp()    
+//         comingSoon('#316436', 'Felipa')   
+//         appTitle.style.color = '#316436';
+//         appTitle.style.fontFamily = 'Felipa', cursive;
+//     } else if (selectedApp === 'shop') {
+//         cleanUp()   
+//         comingSoon('#b95358', 'Major Mono Display')    
+//         appTitle.style.color = '#b95358';
+//         appTitle.style.fontFamily = 'Major Mono Display', monospace;
+//     } else if (selectedApp === 'note') {
+//         cleanUp()   
+//         comingSoon('#7dcfa8', 'Fredericka the Great')    
+//         appTitle.style.color = '#7dcfa8';
+//         appTitle.style.fontFamily = 'Fredericka the Great', cursive;
+//     } else if (selectedApp === 'grave') {
+//         cleanUp()   
+//         comingSoon('#0e0e0e', 'Creepster')    
+//         appTitle.style.color = '#0e0e0e';
+//         appTitle.style.fontFamily = 'Creepster', cursive;
+//     } else {
+//         cleanUp();
+//     }
+// }
+
+const render = () => {
+    cleanUp();
+    switch(appList[0].name) {
+        case('game'):
+            generateGame(appList[0].color);
+            break;
+        case('art'):
+            generateArt(appList[0].color);
+            break;
+        case('grave'):
+            generateGrave();
+            break;
+        default: 
+            return;
+    }
 }
 
 const setDisplay = () => {
     let selectedApp = appList[0];
-    appTitle.innerHTML = selectedApp;
-    if (selectedApp === 'art') {
-        cleanUp()
-        generateArt('#5433be');
-        appTitle.style.color = '#5433be';
-        appTitle.style.fontFamily = 'Permanent Marker', cursive;
-    } else if (selectedApp === 'game') {
-        cleanUp()
-        generateGame('#e624af');
-        appTitle.style.color = '#e624af';
-        appTitle.style.fontFamily = 'DotGothic16', sans-serif; 
-    } else if (selectedApp === 'app') {
-        cleanUp()      
-         comingSoon('#3df9ea', 'Ropa Sans')
-        appTitle.style.color = '#3df9ea';
-        appTitle.style.fontFamily = 'Ropa Sans', sans-serif;
-    } else if (selectedApp === 'blog') {
-        cleanUp()    
-        comingSoon('#316436', 'Felipa')   
-        appTitle.style.color = '#316436';
-        appTitle.style.fontFamily = 'Felipa', cursive;
-    } else if (selectedApp === 'shop') {
-        cleanUp()   
-        comingSoon('#b95358', 'Major Mono Display')    
-        appTitle.style.color = '#b95358';
-        appTitle.style.fontFamily = 'Major Mono Display', monospace;
-    } else if (selectedApp === 'note') {
-        cleanUp()   
-        comingSoon('#7dcfa8', 'Fredericka the Great')    
-        appTitle.style.color = '#7dcfa8';
-        appTitle.style.fontFamily = 'Fredericka the Great', cursive;
-    } else if (selectedApp === 'grave') {
-        cleanUp()   
-        comingSoon('#0e0e0e', 'Creepster')    
-        appTitle.style.color = '#0e0e0e';
-        appTitle.style.fontFamily = 'Creepster', cursive;
-    } else {
-        cleanUp();
-    }
+    appTitle.innerHTML = selectedApp.name;
+    appTitle.style.color = selectedApp.color
+    render();
 }
 
 // assign button functionality
